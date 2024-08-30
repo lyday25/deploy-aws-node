bf8e0ee405ceb7272f8dbf3cb58e99cc2e11107e
lyday25 <bilqisalayaki@gmail.com>
2024-08-30 19:50:08 +0100

    modified files

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('welcome to my world ');
+  res.send('welcome to my website ');
+});
+app.get('/next', function (req, res) {
+  res.send('{ "response": "yes this is the next page,please register your account"}');
+});
+app.get('/ok', function (req, res) {
+  res.send('{ "response": " Nice Job!, cool job and welldone!" }');
 });
 
 app.listen(PORT, HOST);
